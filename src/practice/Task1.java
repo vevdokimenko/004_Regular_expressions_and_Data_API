53bea512f811ea0fe2e65228bc67903bf00cb79e
vevdokimenko <evdokimenko@hotmail.com>
2021-11-06 12:12:31 +0200

    Task1

--- src/practice/Task1.java
+++ src/practice/Task1.java
@@ -1,2 +1,23 @@
-package practice;public class Task1 {
+package practice;
+
+/*
+*Задание 1
+Дана строка 'ahb acb aeb aeeb adcb axeb'. Напишите регулярное выражение, которая найдет строки ahb, acb, aeb по
+шаблону: буква 'a', любой символ, буква 'b'. Выведите строки на консоль
+* */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String str = "ahb acb aeb aeeb adcb axeb";
+
+        Pattern pattern = Pattern.compile("a.b");
+        Matcher matcher = pattern.matcher(str);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
 }
